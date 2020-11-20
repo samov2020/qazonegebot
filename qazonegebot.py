8ecfcc6c673b56466cb0c773bc011efbc36eb834
samov2020 <72760151+samov2020@users.noreply.github.com>
2020-11-20 22:53:58 +0600

    Update qazonegebot.py

--- qazonegebot.py
+++ qazonegebot.py
@@ -124,7 +124,7 @@ def getMessage():
 @server.route("/")
 def webhook():
     bot.remove_webhook()
-    bot.set_webhook(url='https://qazonegebot.com/' + TOKEN)
+    bot.set_webhook(url='https://qazonegebot.herokuapp.com/' + TOKEN)
     return "!", 200
 
 if __name__ == "__main__":
