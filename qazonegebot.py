213e514844ebf7a03a51dedc6afbd856f3412fe6
samov2020 <72760151+samov2020@users.noreply.github.com>
2020-11-20 21:12:00 +0600

    Update qazonegebot.py

--- qazonegebot.py
+++ qazonegebot.py
@@ -136,7 +136,4 @@ bot.set_webhook(url=WEBHOOK_URL_BASE + WEBHOOK_URL_PATH,
                 certificate=open(WEBHOOK_SSL_CERT, 'r'))
 
 # Start flask server
-app.run(host=WEBHOOK_LISTEN,
-        port=WEBHOOK_PORT,
-        ssl_context=(WEBHOOK_SSL_CERT, WEBHOOK_SSL_PRIV),
-        debug=True)v
+app.run(host=WEBHOOK_LISTEN, port=WEBHOOK_PORT, ssl_context=(WEBHOOK_SSL_CERT, WEBHOOK_SSL_PRIV), debug = True)
